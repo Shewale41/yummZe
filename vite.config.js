f642c1e615ed203b5b9e7c01da126e280348e433
Shewale41 <shewalegopal4119@gmail.com>
2025-05-12 21:46:46 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/yummZe/', 
   plugins: [react(), tailwindcss()],
   css: {
-    devSourcemap: false, // Disable CSS analysis
+    devSourcemap: false,
   },
 })
